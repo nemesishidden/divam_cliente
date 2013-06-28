a3088966e4077c665450475b011aa6bd450e7f5d
nemesishidden <nemesishidden@gmail.com>
2013-06-28 09:32:41 -0400

    Cambios en base de datos

--- controller/controller.js
+++ controller/controller.js
@@ -30,11 +30,11 @@ var app = {
 
     receivedEvent: function(id) {
         // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Evento Recivido: ' + id);
     },
